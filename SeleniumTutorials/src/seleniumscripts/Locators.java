e3287244f084a0d3e1191ae848f8624c57bf2a5a
krrahu <rahul.baliyase91@gmail.com>
2024-11-03 18:02:52 +0530

    test4

--- SeleniumTutorials/src/seleniumscripts/Locators.java
+++ SeleniumTutorials/src/seleniumscripts/Locators.java
@@ -25,40 +25,61 @@ public class Locators {
 				driver.get("https://rahulshettyacademy.com/locatorspractice/");
 				System.out.println(driver.getTitle());
 				System.out.println(driver.getCurrentUrl());
-				// id Locator
+				
+				// id Locator (step 1)
 				driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 				// name Locator
 				driver.findElement(By.name("inputPassword")).sendKeys("hello123");
-				// class name Locator
+				// class name Locator (step 1)
 				driver.findElement(By.className("signInBtn")).click();
-		          System.out.println(driver.findElement(By.cssSelector("p.error")).getText()); // when crendial is wrong 
-				System.out.println("login un sucessfully");
+				// css locator ((step 1)
+		   System.out.println(driver.findElement(By.cssSelector("p.error")).getText()); // when crendial is wrong 
+				System.out.println("login unsucessfully due to inccorect PWD or UN"); 
 				
-				// link text Locator
+				// link text Locator (step 2)
 				driver.findElement(By.linkText("Forgot your password?")).click();
-				System.out.println("naviagte to forgot password page");
+				System.out.println("naviagte to forgot password screen");
 				
-				// X-path and CSS  Locator
+				// X-path and CSS  Locator (step 3)
 				driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("test");
 				driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("test@gmail.com");
-				
 				driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("98878798798");
-			System.out.println(driver.findElement(By.className("reset-pwd-btn")).getText());
-				System.out.println("clicking the reset button ");
-				driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
-				System.out.println(driver.findElement(By.className("infoMsg")).getText());
 				
+				// class name Locator (step 4)
+				driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+				
+			    System.out.println(driver.findElement(By.className("infoMsg")).getText()); // error message
+			    System.out.println(" reset button clicked and error messge captuured ");
+			    
+				// storing text of reset login button -some extra validation
+			    System.out.println(driver.findElement(By.className("reset-pwd-btn")).getText()); 
+			    // some extra validation (
 				driver.findElement(By.xpath("//input[@type='text'][3]")).clear();
 				driver.findElement(By.cssSelector("input[type='text']:nth-child(5)")).sendKeys("99999999");
 				driver.findElement(By.cssSelector("input[type='text']:nth-child(5)")).clear();
 				driver.findElement(By.xpath("//form/input[3]")).sendKeys("12121212");
 				
+				// xpath Locator (step 4- click on Go to login page button)
+				
+				driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+				Thread.sleep(2000);
 				
 				
-				//System.out.println("login sucessfully");
+				// step 6:- entered correct UN and PWD > select Remember my username check box > click on sign
 				
-				//driver.quit();
-				//System.out.println("browsr is closed");
+				driver.findElement(By.cssSelector("input[placeholder='Username']")).sendKeys("rahul");
+				//driver.findElement(By.cssSelector("Input[type*=’pass’]")).sendKeys("rahulshettyacademy"); // partial text
+				driver.findElement(By.cssSelector("input[name='inputPassword']")).sendKeys("rahulshettyacademy");
+				driver.findElement(By.cssSelector("#chkboxOne")).click();
+				driver.findElement(By.xpath("//button[@class='submit signInBtn']")).click();
+				//driver.findElement(By.className("signInBtn")).click();
+				
+				System.out.println("login sucessfully");
+				
+				// step 7:-  logout 
+				driver.findElement(By.className("logout-btn")).click();
+                // driver.quit();
+				// System.out.println("browsr is closed");
 			
 			
 
